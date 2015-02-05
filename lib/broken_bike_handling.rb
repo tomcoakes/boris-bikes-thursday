48541621921c34ae9a9d0b105e52891250d1d76a
Tom Coakes <tomcoakes@gmail.com>
2015-02-05 20:26:10 +0000

    Checked that broken_bike_release method of the broken_bike_handling module.

--- lib/broken_bike_handling.rb
+++ lib/broken_bike_handling.rb
@@ -4,4 +4,14 @@ module BrokenBikeHandling
 		bike_rack.select {|bike| bike.broken?}.count
 	end
 
+  def broken_bike_release
+    bike_rack.select do |bike|
+      broken_bikes = []
+      if bike.broken?
+        broken_bikes << bike
+        bike_rack.delete(bike)
+      end
+    end
+  end
+
 end
--- spec/broken_bike_handling_spec.rb
+++ spec/broken_bike_handling_spec.rb
@@ -10,23 +10,27 @@ end
 
 describe BrokenBikeHandling do
 
-
-
+	let(:holder) {Holder.new}
 	let(:working_bike) {double :working_bike, broken?: false}
 	let(:broken_bike) {double :broken_bike, broken?: true}
 
-
 	it 'should know if it has any broken bikes' do
-		holder = Holder.new
-		expect(holder.broken_bike_count).to eq(0)
 		holder.dock(working_bike)
+		holder.dock(broken_bike)
+		expect(holder.broken_bike_count).to eq(1)
+	end
+
+	it "should be able to release broken bikes" do
 		expect(holder.broken_bike_count).to eq(0)
 		holder.dock(broken_bike)
 		expect(holder.broken_bike_count).to eq(1)
-
+		holder.broken_bike_release
+		expect(holder.broken_bike_count).to eq(0)
 	end
 
-
+	it "should know which bikes are broken" do
+		
+	end
 
 end
 
