39d11bbcb689a94ce784291098d3ff2ed9b31bb8
Tom Coakes <tomcoakes@gmail.com>
2015-02-05 18:50:17 +0000

    Added more methods to BikeContainer and tested them.

--- lib/bike_container.rb
+++ lib/bike_container.rb
@@ -1,5 +1,7 @@
 module BikeContainer
 
+  attr_reader :capacity
+
 	def bike_rack
 		@bike_rack ||= []
 	end
@@ -8,4 +10,20 @@ module BikeContainer
 		bike_rack << bike
 	end 
 	
+  def bike_rack_capacity
+    @capacity = 20
+  end
+
+  def full?
+    true
+  end
+
+  def available_bikes
+    bike_rack.count
+  end
+
+  def release
+    bike_rack.pop
+  end
+
 end
--- spec/bike_container_spec.rb
+++ spec/bike_container_spec.rb
@@ -7,16 +7,42 @@ describe BikeContainer do
 let(:holder) {Holder.new}
 let(:bike) {double :bike}
 
+def add_one_bike
+  holder.dock(bike)
+end
+
 	it 'should be able to hold bikes' do
 		expect(holder.bike_rack).to eq []
 	end
 
 	it 'should be able to dock bikes' do
 		expect(holder.bike_rack.count).to eq(0)
-		holder.dock(bike) 
+		add_one_bike 
 		expect(holder.bike_rack.count).to eq(1)
 	end
 
-	
+	it "should know its capacity" do
+    expect(holder.bike_rack_capacity).to eq(20)
+  end
+
+  it "should know when its full" do
+    expect(holder.bike_rack.count).to eq(0)
+    20.times {add_one_bike}
+    expect(holder.full?).to eq true
+  end
+
+  it "should know how many available bikes there are" do
+    expect(holder.bike_rack.count).to eq(0)
+    add_one_bike 
+    expect(holder.available_bikes).to eq(1)
+  end
+
+  it "should be able to release bikes" do
+    expect(holder.bike_rack.count).to eq(0)
+    add_one_bike
+    holder.release
+    expect(holder.bike_rack.count).to eq(0)
+
+  end
 
 end
